946447011f3572508b8350c1005a9a0946351309
Kelvin Yan <keyan@Kelvins-MacBook-Pro.local>
2017-07-10 00:53:37 -0700

    forgot to add these

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
